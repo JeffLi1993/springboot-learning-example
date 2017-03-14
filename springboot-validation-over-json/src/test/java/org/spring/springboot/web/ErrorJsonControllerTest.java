caf2e86356c414340710a8da0bb6ab844d9edffa
JeffLi1993 <qiangqiangli1993@gmail.com>
2017-03-14 11:53:15 +0800

    Spring Boot HTTP over JSON 的错误码异常处理

---
+++ springboot-validation-over-json/src/test/java/org/spring/springboot/web/ErrorJsonControllerTest.java
@@ -0,0 +1,15 @@
+package org.spring.springboot.web;
+
+import org.junit.Test;
+
+/**
+ * Spring Boot ErrorJsonController 测试 - {@link ErrorJsonController}
+ *
+ * Created by bysocket on 16/4/26.
+ */
+public class ErrorJsonControllerTest {
+
+    @Test
+    public void testSayHello() {
+    }
+}
