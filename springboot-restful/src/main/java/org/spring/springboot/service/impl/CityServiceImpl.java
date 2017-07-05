6400d65561c41b6932d5c7549aadbdd77c2fbfb0
FrankieYang <andypeker@163.com>
2017-07-05 21:58:32 +0800

    可以运行起来。。。

--- springboot-restful/src/main/java/org/spring/springboot/service/impl/CityServiceImpl.java
+++ springboot-restful/src/main/java/org/spring/springboot/service/impl/CityServiceImpl.java
@@ -5,6 +5,7 @@ import org.spring.springboot.domain.City;
 import org.spring.springboot.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,26 +22,31 @@ public class CityServiceImpl implements CityService {
     @Autowired
     private CityDao cityDao;
 
+    @Transactional
     public List<City> findAllCity(){
         out.println("\n\nCityRestController ServiceImple\n\n");
         return cityDao.findAllCity();
     }
 
+    @Transactional
     public City findCityById(Long id) {
         return cityDao.findById(id);
     }
 
     @Override
+    @Transactional
     public Long saveCity(City city) {
         return cityDao.saveCity(city);
     }
 
     @Override
+    @Transactional
     public Long updateCity(City city) {
         return cityDao.updateCity(city);
     }
 
     @Override
+    @Transactional
     public Long deleteCity(Long id) {
         return cityDao.deleteCity(id);
     }
