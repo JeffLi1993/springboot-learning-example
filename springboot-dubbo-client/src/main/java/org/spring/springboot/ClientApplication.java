93e3f0898bdadd7578174a12e3f5f40d5f55cf0a
FrankieYang <andypeker@163.com>
2017-07-05 21:23:41 +0800

    可以运行起来。。。

--- springboot-dubbo-client/src/main/java/org/spring/springboot/ClientApplication.java
+++ springboot-dubbo-client/src/main/java/org/spring/springboot/ClientApplication.java
@@ -1,8 +1,14 @@
 package org.spring.springboot;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import org.spring.springboot.dubbo.CityDubboConsumerService;
+import org.spring.springboot.dubbo.CityDubboService;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 /**
@@ -12,13 +18,27 @@ import org.springframework.context.ConfigurableApplicationContext;
  */
 // Spring Boot 应用的标识
 @SpringBootApplication
-public class ClientApplication {
+public class ClientApplication implements EmbeddedServletContainerCustomizer {
 
     public static void main(String[] args) {
         // 程序启动入口
         // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
-        ConfigurableApplicationContext run = SpringApplication.run(ClientApplication.class, args);
-        CityDubboConsumerService cityService = run.getBean(CityDubboConsumerService.class);
-        cityService.printCity();
+
+        try {
+            ConfigurableApplicationContext run = SpringApplication.run(ClientApplication.class, args);
+
+            CityDubboConsumerService cityService = run.getBean(CityDubboConsumerService.class);
+//            CityDubboConsumerService cityService = run.getBean("cityDubboService");
+
+            cityService.printCity();
+
+        } catch (Exception e){}
+
     }
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(8082);
+    }
+
 }
