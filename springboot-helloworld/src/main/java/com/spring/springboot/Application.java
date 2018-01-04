7aa69a0c77a496599981d82c057e5fc4a1c1a7a7
frankie <franklinyang2013@gmail.com>
2018-01-04 11:50:07 +0800

    21312

--- springboot-helloworld/src/main/java/com/spring/springboot/Application.java
+++ springboot-helloworld/src/main/java/com/spring/springboot/Application.java
@@ -1,9 +1,34 @@
 package com.spring.springboot;
 
+import com.spring.springboot.listener.*;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.logging.AutoConfigurationReportLoggingInitializer;
+import org.springframework.boot.builder.ParentContextCloserApplicationListener;
+import org.springframework.boot.context.FileEncodingApplicationListener;
+import org.springframework.boot.context.config.AnsiOutputApplicationListener;
+import org.springframework.boot.context.config.ConfigFileApplicationListener;
+import org.springframework.boot.context.config.DelegatingApplicationListener;
+import org.springframework.boot.context.embedded.ServerPortInfoApplicationContextInitializer;
+import org.springframework.boot.context.event.EventPublishingRunListener;
+import org.springframework.boot.liquibase.LiquibaseServiceLocatorApplicationListener;
+import org.springframework.boot.logging.ClasspathLoggingApplicationListener;
+import org.springframework.boot.logging.LoggingApplicationListener;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.AbstractApplicationEventMulticaster;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.io.support.SpringFactoriesLoader;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContainerInitializer;
 
 /**
  * Spring Boot 应用启动类
@@ -17,8 +42,16 @@ public class Application {
     public static void main(String[] args) {
         // 程序启动入口
         // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
-        SpringApplication.run(Application.class,args);
+//        SpringApplication.run(Application.class, args);
         SpringApplication app = new SpringApplication(Application.class);
+
+        app.addListeners(new MyApplicationStartedListener(),
+                new MyApplicationFailListener(),
+                new MyApplicationReadyListener(),
+                new MyApplicationPreparedListener(),
+                new MyEnvReadyListener());
+
+        app.run(args);
     }
 
 
@@ -26,6 +59,54 @@ public class Application {
 
         ServletContextInitializer ewfwef;
         WebApplicationInitializer wefwefwef;
+
+
+        SpringBootConfiguration wer;
+        EnableAutoConfiguration wrwer23;
+
+        EnableScheduling we3r;
+        EnableCaching werwer;
+
+        SpringFactoriesLoader wer2323r23r;
+
+
+        SpringApplicationRunListener wewerl;
+        ApplicationListener fwefwef;
+
+        ParentContextCloserApplicationListener ewf;
+        FileEncodingApplicationListener fwefwe;
+        AnsiOutputApplicationListener fwef;
+        ConfigFileApplicationListener fwefewf;
+        DelegatingApplicationListener ddddergerd;
+        LiquibaseServiceLocatorApplicationListener gwgwegweg;
+        ClasspathLoggingApplicationListener fwefwewefwef;
+        LoggingApplicationListener fwefweflwefl;
+
+
+
+        //  org.springframework.context
+        ApplicationContextInitializer wefwefwwwef;
+            AutoConfigurationReportLoggingInitializer vnbvg;
+            ServerPortInfoApplicationContextInitializer fefwefewf;
+        //  javax.servlet
+        ServletContainerInitializer svltContIniter;
+        //  org.springframework.web
+        WebApplicationInitializer webAppIniter;
+        //  org.springframework.boot.web.servlet
+        ServletContextInitializer svltCntxIniter;
+
+
+
+//        SpringApplicationRunListeners springAppRunLsnrs;
+        SpringApplicationRunListener springAppRunLsnr;
+            EventPublishingRunListener ergergerg;
+        ApplicationEventMulticaster gergerg;
+            AbstractApplicationEventMulticaster ewwefwef;
+                SimpleApplicationEventMulticaster sdgweg;
+        ApplicationListener lsnr;
+//            rergerg ergerg;
+//                rergerg ergerg;
+
 
     }
 
---
+++ springboot-helloworld/src/main/java/com/spring/springboot/listener/MyEnvReadyListener.java
@@ -0,0 +1,15 @@
+package com.spring.springboot.listener;
+
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.context.ApplicationListener;
+
+/**
+ * @author Frankie Yang on 2018/1/4.
+ */
+
+public class MyEnvReadyListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
+        System.out.println("Frankie Yang 的 Environment ready 啦！！！");
+    }
+}
