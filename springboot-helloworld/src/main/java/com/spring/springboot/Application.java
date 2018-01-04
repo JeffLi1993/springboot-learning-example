c355eb5d618b64f7c6d46f061c3fed84124ca174
franklin <andypeker@163.com>
2018-01-04 21:05:56 +0800

    regergerger

--- springboot-helloworld/src/main/java/com/spring/springboot/Application.java
+++ springboot-helloworld/src/main/java/com/spring/springboot/Application.java
@@ -1,9 +1,22 @@
 package com.spring.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.config.AnsiOutputApplicationListener;
+import org.springframework.boot.context.config.ConfigFileApplicationListener;
+import org.springframework.boot.context.config.DelegatingApplicationListener;
+import org.springframework.boot.liquibase.LiquibaseServiceLocatorApplicationListener;
+import org.springframework.boot.logging.ClasspathLoggingApplicationListener;
+import org.springframework.boot.logging.LoggingApplicationListener;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContainerInitializer;
 
 /**
  * Spring Boot 应用启动类
@@ -25,7 +38,26 @@ public class Application {
     public void fwefwef(){
 
         ServletContextInitializer ewfwef;
-        WebApplicationInitializer wefwefwef;
+
+        WebApplicationInitializer webIniter;
+        ServletContainerInitializer svltContIniter;
+
+        ApplicationContextInitializer appCntxIniter;
+
+
+        SpringBootServletInitializer bootSvltIniter;
+
+
+        ApplicationListener appLsnr;
+            LoggingApplicationListener lggAppLsnr;
+            ClasspathLoggingApplicationListener classPathLggAppLsnr;
+            LiquibaseServiceLocatorApplicationListener liquiBaseSvcLocAppLsnr;
+            ConfigFileApplicationListener cfgFileAppLsnr;
+            AnsiOutputApplicationListener ansiOpAppLsnr;
+            DelegatingApplicationListener dlgtAppLsnr;
+
+
+        SpringApplicationRunListener grwegweg;
 
     }
 
