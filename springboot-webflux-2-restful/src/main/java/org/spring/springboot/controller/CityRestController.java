18a2eb1e97d4f2df64addfbd01c9b1e84518a1a7
liqiangqiang <liqiangqiang@youzan.com>
2018-04-08 16:26:20 +0800

    WebFlux RestFul 实现

---
+++ springboot-webflux-2-restful/src/main/java/org/spring/springboot/controller/CityRestController.java
@@ -0,0 +1,51 @@
+package org.spring.springboot.controller;
+
+import org.spring.springboot.domain.City;
+import org.spring.springboot.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * 城市 Controller 实现 Restful HTTP 服务
+ * <p>
+ * Created by bysocket on 09/29/2017.
+ */
+@RestController
+@RequestMapping(value = "/city")
+public class CityRestController {
+
+    @Autowired
+    private CityService cityService;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Mono<City> findOneCity(@PathVariable("id") Long id) {
+        return Mono.create(cityMonoSink -> cityMonoSink.success(cityService.findCityById(id)));
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Flux<City> findAllCity() {
+        return Flux.create(cityFluxSink -> {
+            cityService.findAllCity().forEach(city -> {
+                cityFluxSink.next(city);
+            });
+            cityFluxSink.complete();
+        });
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Mono<Long> createCity(@RequestBody City city) {
+        return Mono.create(cityMonoSink -> cityMonoSink.success(cityService.saveCity(city)));
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public Mono<Long> modifyCity(@RequestBody City city) {
+        return Mono.create(cityMonoSink -> cityMonoSink.success(cityService.updateCity(city)));
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public Mono<Long> modifyCity(@PathVariable("id") Long id) {
+        return Mono.create(cityMonoSink -> cityMonoSink.success(cityService.deleteCity(id)));
+    }
+}
