eb36d3d01dfdb52b6dbf9f034f853b840acf00d5
liqiangqiang <liqiangqiang@youzan.com>
2018-04-10 14:48:16 +0800

    WebFlux Web CRUD 实践

---
+++ springboot-webflux-2-restful/src/main/java/org/spring/springboot/handler/CityHandler.java
@@ -0,0 +1,42 @@
+package org.spring.springboot.handler;
+
+import org.spring.springboot.dao.CityRepository;
+import org.spring.springboot.domain.City;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Component
+public class CityHandler {
+
+    private final CityRepository cityRepository;
+
+    @Autowired
+    public CityHandler(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
+    }
+
+    public Mono<Long> save(City city) {
+        return Mono.create(cityMonoSink -> cityMonoSink.success(cityRepository.save(city)));
+    }
+
+    public Mono<City> findCityById(Long id) {
+        return Mono.create(cityMonoSink -> cityMonoSink.success(cityRepository.findCityById(id)));
+    }
+
+    public Flux<City> findAllCity() {
+        return Flux.create(cityFluxSink -> {
+            cityRepository.findAll().forEach(city -> cityFluxSink.next(city));
+            cityFluxSink.complete();
+        });
+    }
+
+    public Mono<Long> modifyCity(City city) {
+        return Mono.create(cityMonoSink -> cityMonoSink.success(cityRepository.updateCity(city)));
+    }
+
+    public Mono<Long> deleteCity(Long id) {
+        return Mono.create(cityMonoSink -> cityMonoSink.success(cityRepository.deleteCity(id)));
+    }
+}
