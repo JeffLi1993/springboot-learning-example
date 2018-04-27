f2c764414cb621d9b67b6b99ce6f74a2afa05427
liqiangqiang <liqiangqiang@youzan.com>
2018-04-27 16:05:00 +0800

    Spring Boot WebFlux 整合 Redis

---
+++ springboot-webflux-6-redis/src/main/java/org/spring/springboot/Application.java
@@ -0,0 +1,18 @@
+package org.spring.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 应用启动类
+ *
+ */
+// Spring Boot 应用的标识
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        // 程序启动入口
+        SpringApplication.run(Application.class,args);
+    }
+}
