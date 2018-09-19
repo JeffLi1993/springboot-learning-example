baf6249ac0b1c90f1aa3f818145ae8ffc9bc5ebd
frankie <franklinyang2013@gmail.com>
2018-09-19 17:40:10 +0800

    21312

---
+++ springboot-helloworld/src/main/java/com/spring/springboot/initializer/AnnotationsBasedApplicationInitializer.java
@@ -0,0 +1,20 @@
+package com.spring.springboot.initializer;
+
+import org.springframework.web.context.AbstractContextLoaderInitializer;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+public class AnnotationsBasedApplicationInitializer extends AbstractContextLoaderInitializer {
+  
+    @Override
+    protected WebApplicationContext createRootApplicationContext() {
+        AnnotationConfigWebApplicationContext rootContext
+          = new AnnotationConfigWebApplicationContext();
+        rootContext.register(RootApplicationConfig.class);
+        return rootContext;
+    }
+}
+
+class RootApplicationConfig {
+
+}
---
+++ springboot-helloworld/src/main/java/com/spring/springboot/initializer/CfgRootACWebApplicationInitializer.java
@@ -0,0 +1,28 @@
+package com.spring.springboot.initializer;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.AbstractContextLoaderInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+/**
+ * @author Frankie Yang on 2018/9/19.
+ */
+public class CfgRootACWebApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext)
+            throws ServletException {
+
+        XmlWebApplicationContext rootContext = new XmlWebApplicationContext();
+
+        rootContext.setConfigLocations("/WEB-INF/rootApplicationContext.xml");
+
+        servletContext.addListener(new ContextLoaderListener(rootContext));
+
+    }
+
+}
