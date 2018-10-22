f5417b3022e03ff797f0d85904b2701bb97d2dac
frankie <franklinyang2013@gmail.com>
2018-10-22 17:43:09 +0800

    new file:   initializer/CustomServletContainerInitializer2.java

---
+++ springboot-helloworld/src/main/java/com/spring/springboot/initializer/CustomServletContainerInitializer2.java
@@ -0,0 +1,19 @@
+package com.spring.springboot.initializer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.Set;
+
+/**
+ * @author Frankie Yang on 2018/8/23.
+ */
+public class CustomServletContainerInitializer2 implements ServletContainerInitializer {
+    @Override
+    public void onStartup(Set<Class<?>> scls, ServletContext sc) throws ServletException {
+        System.out.println("~~~~~~~~~~~~~~~[ServletContainerInitializer]frankie Initializer: Server Info =====" + sc.getServerInfo() + "\t" + sc.getServletContextName());
+    }
+}
