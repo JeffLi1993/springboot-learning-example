95c76fabfdc227deebe0198539bc9a398f824b79
liqiangqiang <liqiangqiang@youzan.com>
2019-04-12 01:12:50 +0800

    WebFlux REST API 全局异常处理  Error Handling

---
+++ 2-x-spring-boot-webflux-handling-errors/src/main/java/org/spring/springboot/error/GlobalErrorAttributes.java
@@ -0,0 +1,32 @@
+package org.spring.springboot.error;
+
+import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+
+import java.util.Map;
+
+@Component
+public class GlobalErrorAttributes extends DefaultErrorAttributes {
+    
+    @Override
+    public Map<String, Object> getErrorAttributes(ServerRequest request, boolean includeStackTrace) {
+        Map<String, Object> map = super.getErrorAttributes(request, includeStackTrace);
+    
+        if (getError(request) instanceof GlobalException) {
+            GlobalException ex = (GlobalException) getError(request);
+            map.put("exception", ex.getClass().getSimpleName());
+            map.put("message", ex.getMessage());
+            map.put("status", ex.getStatus().value());
+            map.put("error", ex.getStatus().getReasonPhrase());
+            
+            return map;
+        }
+    
+        map.put("exception", "SystemException");
+        map.put("message", "System Error , Check logs!");
+        map.put("status", "500");
+        map.put("error", " System Error ");
+        return map;
+    }
+}
